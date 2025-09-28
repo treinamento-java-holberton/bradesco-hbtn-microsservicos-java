afe78353f0bbe43cbca82100a9f79f7d5a6dcf5a
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-09-28 18:41:20 -0300

    0. Construindo uma API REST com Spring Boot com validação e tratamento de exceções - USER API

--- 0x06/api-rest-userController/demo/src/main/java/com/example/demo/DemoApplication.java
+++ 0x06/api-rest-userController/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,11 @@
-package main.java.com.example.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.demo")
 public class DemoApplication {
 
     public static void main(String[] args) {
